f892a4ba5514babe6501940a338d520b79a3d078
Rubén <ruben.crispin@hotmail.com>
2018-09-28 17:27:31 +0200

    Created TextureHolder instance for uploading each texture only once. Created Zombie class and functions to manage them. Coded CreateHorde function and used in the main loop for spawning a zombie horde.

--- ZombieArena/ZombieArena.h
+++ ZombieArena/ZombieArena.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "Zombie.h" 
 //#include "Player.cpp"
-//using namespace sf;
-//int createBackground(VertexArray& rVA, IntRect arena);
+using namespace sf;
+int createBackground(VertexArray& rVA, IntRect arena);
+Zombie* createHorde(int numZombies, IntRect arena);
